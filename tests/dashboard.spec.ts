c81710dbda7b17d29d349cc7233f41b0056f3e3e
Patrick Blood <pblood66@gmail.com>
2026-02-08 12:36:50 -0700

    add load admin dashboard test

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -1,5 +1,18 @@
 import { test, expect } from "playwright-test-coverage";
 import { basicInit } from "./testUtils";
+import { Role } from '../src/service/pizzaService';
+
+test('admin dashboard loads', async ({ page }) => {
+    await basicInit(page, Role.Admin);
+
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByPlaceholder('Email address').fill('d@jwt.com');
+    await page.getByPlaceholder('Password').fill('a');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await page.getByRole('link', { name: /Admin/i }).click();
+    await expect(page.getByRole('list')).toContainText('admin-dashboard');
+});
 
 test("diner dashboard loads", async ({ page }) => {
     await basicInit(page);
