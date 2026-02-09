ff4ed73e1c47c9112b3658fc85b9fbba93ce3bc2
Patrick Blood <pblood66@gmail.com>
2026-02-09 10:57:09 -0700

    Add Franchisee dashboard test

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -25,3 +25,15 @@ test("diner dashboard loads", async ({ page }) => {
     await page.getByRole('link', { name: 'kc' }).click();
     await expect(page.getByRole('list')).toContainText('diner-dashboard');
 });
+
+test("franchise dashboard loads", async ({ page }) => {
+    await basicInit(page, Role.Franchisee);
+
+    await page.getByRole("link", { name: "Login" }).click();
+    await page.getByPlaceholder("Email address").fill("d@jwt.com");
+    await page.getByPlaceholder("Password").fill("a");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await page.getByLabel('Global').getByRole('link', { name: 'Franchise' }).click();
+    await expect(page.getByRole('list')).toContainText('franchise-dashboard');
+});
