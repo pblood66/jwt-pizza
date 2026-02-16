1bc568ea065a7ca5d3e936b70f677fefce8b7ec9
Patrick Blood <pblood66@gmail.com>
2026-02-15 17:06:24 -0700

    renamed auth tests for users

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "playwright-test-coverage";
 import { basicInit } from "./testUtils";
-
+import { Role } from "../src/service/pizzaService";
 
 test("home page", async ({ page }) => {
   await page.goto("/");
@@ -8,50 +8,51 @@ test("home page", async ({ page }) => {
   expect(await page.title()).toBe("JWT Pizza");
 });
 
-test('login', async ({ page }) => {
+test("login", async ({ page }) => {
   await basicInit(page);
 
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
-  await page.getByRole('textbox', { name: 'Password' }).fill('a');
-  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).fill("d@jwt.com");
+  await page.getByRole("textbox", { name: "Password" }).fill("a");
+  await page.getByRole("button", { name: "Login" }).click();
 
-  await expect(page.getByRole('link', { name: 'KC' })).toBeVisible();
+  await expect(page.getByRole("link", { name: "KC" })).toBeVisible();
 });
 
-test('user registration', async ({ page }) => {
+test("user registration", async ({ page }) => {
   await basicInit(page);
 
-  await page.getByRole('link', { name: 'Register' }).click();
+  await page.getByRole("link", { name: "Register" }).click();
 
-  await page.getByPlaceholder('Full name').fill('New User');
-  await page.getByPlaceholder('Email address').fill('new@jwt.com');
-  await page.getByPlaceholder('Password').fill('a');
+  await page.getByPlaceholder("Full name").fill("New User");
+  await page.getByPlaceholder("Email address").fill("new@jwt.com");
+  await page.getByPlaceholder("Password").fill("a");
 
-  await page.route('*/**/api/auth', async route => {
-    expect(route.request().method()).toBe('POST');
+  await page.route("*/**/api/auth", async (route) => {
+    expect(route.request().method()).toBe("POST");
     await route.fulfill({
-      json: { user: { name: 'New User' }, token: 'abc' }
+      json: { user: { name: "New User" }, token: "abc" },
     });
   });
 
-  await page.getByRole('button', { name: 'Register' }).click();
+  await page.getByRole("button", { name: "Register" }).click();
 
-  await expect(page.getByRole('link', { name: 'NU', exact: true })).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "NU", exact: true }),
+  ).toBeVisible();
 });
 
-
-test('logout works', async ({ page }) => {
+test("logout works", async ({ page }) => {
   await basicInit(page);
 
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByPlaceholder('Email address').fill('d@jwt.com');
-  await page.getByPlaceholder('Password').fill('a');
-  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByPlaceholder("Email address").fill("d@jwt.com");
+  await page.getByPlaceholder("Password").fill("a");
+  await page.getByRole("button", { name: "Login" }).click();
 
-  await page.getByRole('link', { name: 'Logout' }).click();
+  await page.getByRole("link", { name: "Logout" }).click();
 
-  await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Login" })).toBeVisible();
 });
 
 test("updateUser", async ({ page }) => {
